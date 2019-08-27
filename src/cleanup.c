686dd7f55b9050bc166fce5b2d3ee20f8496c730
poemdistance <poemdistance@gmail.com>
2019-08-27 22:17:14 +0800

    Kill the google & baidu translation child process before exit

--- src/cleanup.c
+++ src/cleanup.c
@@ -15,12 +15,19 @@ extern char *lastText;
 extern char *text;
 extern int fd_key;
 
+extern pid_t baidu_translate_pid;
+extern pid_t google_translate_pid;
+
+extern int BAIDU_TRANS_EXIT_FALG;
+extern int GOOGLE_TRANS_EXIT_FLAG;
+
+/* TODO:可能会被多次执行，如在终端输入ctrl-c，会被主函数注册的监听SIGINT捕捉到，
+ * 之后python翻译程序退出，又接收到SIGCHLD，再被调用一次，最后StranMonitor程序
+ * 发送SIGTERM，接收到后又调用一次, 不过影响不大*/
+
 void quit() {
 
-    printf("clean up...\n");
-
-    /*退出前加个回车*/
-    fprintf(stdout, "\n");
+    printf("\033[0;35m\nCleaning up... \033[0m\n\n");
 
     if ( text != NULL )
         free(text);
@@ -31,24 +38,27 @@ void quit() {
     close(mousefd);
     close(fd_key);
 
+    /* 清除与谷歌翻译的共享内存*/
     if ( shmdt(shmaddr_google) < 0)
         err_exit("shmdt error");
 
-    printf("\n");
     if (shmctl(shmid_google, IPC_RMID, NULL) == -1)
         err_exit("shmctl error");
-    else {
-        printf("remove shared memory identifier successful (google)\n");
-    }
+    else
+        printf("\033[0;32mremove shared memory identifier successful (google)\033[0m\n");
 
+
+
+    /* 清除与百度翻译的共享内存*/
     if ( shmdt(shmaddr_baidu) < 0)
         err_exit("shmdt error");
 
     if (shmctl(shmid_baidu, IPC_RMID, NULL) == -1)
         err_exit("shmctl error");
-    else {
-        printf("remove shared memory identifier successful (baidu)\n");
-    }
+    else
+        printf("\033[0;32mremove shared memory identifier successful (baidu)\033[0m\n");
+
+
 
     if ( baidu_result[0] != NULL)
         for (int i=0; i<BAIDUSIZE; i++)
@@ -58,7 +68,12 @@ void quit() {
         for (int i=0; i<GOOGLESIZE; i++)
             free(google_result[i]);
 
-    printf("\n");
 
-    exit(1);
+    if ( BAIDU_TRANS_EXIT_FALG != 1 )
+        kill ( baidu_translate_pid, SIGKILL );
+    if ( GOOGLE_TRANS_EXIT_FLAG != 1 )
+        kill ( google_translate_pid, SIGKILL );
+
+    printf("\n");
+    exit(0);
 }
