5f26911c5be42b87f9450d189c9cadedf0cbd661
poemdistance <poemdistance@gmail.com>
2019-08-29 17:26:33 +0800

    Finish some automatic works

--- src/prepare.sh
+++ src/prepare.sh
@@ -2,11 +2,13 @@
 
 #复制文件到家目录
 storage="$HOME/.stran"
-currFile=("../gif_pic/background.jpg" "../gif_pic/tran.png" "../gif_pic/Switch.png")
+currFile=("../gif_pic/background.jpg" "../gif_pic/tran.png" "../gif_pic/Switch.png" 
+    "../gif_pic/volume.png" "./startup.sh" "./errNotification.sh" )
 
 declare -i len
 len=${#currFile[*]}-1
 
+echo
 mkdir $storage
 echo
 for i in $(seq 0 $len)
@@ -14,18 +16,27 @@ do
     cp ${currFile[i]} $storage -v
 done
 
-#修改文件路径
-needChangFile=("./newWindow.c" "./GuiEntrance.c")
+#修改资源文件路径以适应当前用户
+needChangFile=("./newWindow.c" "./GuiEntrance.c" "audioPlayer.c" "Mstran.desktop")
 len=${#needChangFile[@]}-1
-src="\/home\/rease"
-dst="\/"$HOME
+src="/home/rease"
+dst=$HOME
+
+echo
 
 for i in $(seq 0 $len)
 do
-    echo
-    sed -n 's/$src/$dst/p' ${needChangFile[i]}
-    echo "Changing the path to adapt the current user..."
+    
+    sed -i "s~${src}~${dst}~g" ${needChangFile[i]}
+    echo "Changing ${needChangFile[i]} to adapt the current user successful"
+
 done
+
+#复制启动图标，创建日志文件,修改读写权限
+sudo cp Mstran.desktop /usr/share/applications/ -v
+sudo touch /var/log/mstran.log
+sudo chmod -c 750 /var/log/mstran.log
+sudo chown -c $USER /var/log/mstran.log
 
 #加入设备文件用户组
 group=`ls -l /dev/input/mice | awk '{print $4}' | xargs`
@@ -38,35 +49,8 @@ then
     echo "/dev/input/mice is belong to the group of "\"$group\"
     echo "Adding current user to group "\"$group\"
     sudo usermod -aG $group $USER
-fi
 
-#修改键盘设备文件
-device=`cat /var/log/Xorg.0.log | grep keyboard | awk '{print $4}' | tail -n 1 |xargs`
-device2=`cat /var/log/Xorg.1.log | grep keyboard | awk '{print $4}' | tail -n 1 |xargs`
-
-echo
-if [ -n "$device" ] 
-then
-    echo "found the actual keyboard device: /dev/input/"$device
-    actualDevice=device
-fi
-
-if [ -n "$device2" ]
-then
-    echo "found the actual keyboard device: /dev/input/"$device2
-    actualDevice=device2
-fi
-
-if [ -n "$actualDevice" ]
-then
-    src="\/dev\/input\/event3"
-    dst="\/dev\/input\/$actualDevice"
-    inFile="detectMouse.c"
-    sed -i 's/$src/$dst/g' $inFile
-    echo "Modify the source code automatically..."
     echo
-    echo 'All done'
-else
-    echo "Didn't find out the actual keyboard device"
-    echo "Please figure it out by yourself and modify the detectMouse.c"
+    echo "注意: 加入的组在重启后生效，在此之前需要使用sudo执行程序"
+    echo
 fi
