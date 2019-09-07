1eb48c210a126c0d140ae2c152ff78bb76fb701e
poemdistance <poemdistance@gmail.com>
2019-09-07 21:37:45 +0800

    Add codes to detect the event of primary selection

---
+++ src/checkSelectionChanged.c
@@ -0,0 +1,66 @@
+#include "common.h"
+#include <assert.h>
+#include <X11/extensions/Xfixes.h>
+
+extern char *shmaddr_selection;
+
+void WatchSelection(Display *display, const char *bufname);
+
+Display *display;
+
+/* 此函数用不上了，采用sigterm无法kill掉此进程，必须用sigkil，
+ * 但sigkill无法捕捉*/
+void clean() {
+
+    printf("close display\n");
+    if ( display )
+        XCloseDisplay ( display );
+
+    /* 手动置空，双重保险*/
+    display = NULL;
+
+    exit(0);
+}
+
+void checkSelectionChanged(int writefd, int readfd)
+{
+    display = XOpenDisplay(NULL);
+    unsigned long color = BlackPixel(display, DefaultScreen(display));
+    XCreateSimpleWindow(display, DefaultRootWindow(display), 0,0, 1,1, 0, color, color);
+
+    struct sigaction sa;
+    sa.sa_handler =  clean;
+    sigemptyset ( &sa.sa_mask );
+
+    if ( sigaction(SIGTERM, &sa, NULL) == -1) {
+        printf("\033[0;31msigaction err(checkSelectionChanged -> SIGTERM)\033[0m\n");
+        perror("sigaction");
+        quit();
+    }
+
+    if ( sigaction(SIGINT, &sa, NULL) == -1) {
+        printf("\033[0;31msigaction err(checkSelectionChanged -> SIGTERM)\033[0m\n");
+        perror("sigaction");
+        quit();
+    }
+
+    const char buf[] = "PRIMARY";
+    WatchSelection ( display, buf);
+}
+
+void WatchSelection(Display *display, const char *bufname)
+{
+    int event_base, error_base;
+    XEvent event;
+    Atom bufid = XInternAtom(display, bufname, False);
+
+    assert( XFixesQueryExtension(display, &event_base, &error_base) );
+    XFixesSelectSelectionInput(display, DefaultRootWindow(display), bufid, XFixesSetSelectionOwnerNotifyMask);
+
+    while ( 1 ) {
+
+        XNextEvent ( display, &event );
+        shmaddr_selection[0] = '1';
+        printf("selection改变， 写入标志1\n");
+    }
+}
