6a44cf912b5a56c41845b832fa5f4a8d0b252354
poemdistance <poemdistance@gmail.com>
2019-09-09 17:11:02 +0800

    Add codes to make the background picture fitting the actual needed size of GUI

--- src/background.c
+++ src/background.c
@@ -23,6 +23,14 @@ GtkWidget *syncImageSize ( GtkWidget *window, gpointer *data )
     gint width, height;
     gtk_window_get_size ( (GtkWindow*)window, &width, &height );
 
+    if ( ((WinData*)data)->width > width &&  ((WinData*)data)->height > height ) {
+
+        width  = ((WinData*)data)->width ;
+        height  = ((WinData*)data)->height ;
+    }
+    
+    //printf("\033[0;31mSyncImageSize current win size %d %d \033[0m\n", width, height);
+
     /* 销毁原来的背景图片*/
     if ( ((WinData*)data)->oldImage != NULL )
         gtk_widget_destroy ( ((WinData*)data)->oldImage );
