4c074f11ba60b2b2e594647a7faa8f61d674fbff
poemdistance <poemdistance@gmail.com>
2019-09-15 14:19:48 +0800

    Force to kill the processes of mstran's after the death of software

--- src/stop.sh
+++ src/stop.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-kill `ps -aux | grep mstran | head -n 1 |awk '{print $2}'|xargs`
+for i in $(seq 10)
+do
+    kill -9 `ps -aux | grep mstran | head -n 1 |awk '{print $2}'|xargs` >/dev/null 2>&1
+    kill -9 `ps -aux | grep ./stran | head -n 1 |awk '{print $2}'|xargs`>/dev/null 2>&1
+    kill -9 `ps -aux | grep stran | head -n 1 |awk '{print $2}'|xargs`>/dev/null 2>&1
+    kill -9 `ps -aux | grep tranen | head -n 1 |awk '{print $2}'|xargs`>/dev/null 2>&1
+    kill -9 `ps -aux | grep bdtran | head -n 1 |awk '{print $2}'|xargs`>/dev/null 2>&1
+done
+
