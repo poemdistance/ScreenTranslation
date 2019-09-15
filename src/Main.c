33012fc1d852eb1120dcd4a4a010bb9bd77b0446
poemdistance <poemdistance@gmail.com>
2019-09-15 14:00:39 +0800

    Separates the instance functions to other files

--- src/Main.c
+++ src/Main.c
@@ -1,86 +1,23 @@
-/* 注意：
- * 程序退出暂时没有销毁共享内存
- * 等项目后期再来规划相关问题*/
-
 #include "common.h"
 
-char *shmaddr_google;
-char *shmaddr_baidu;
-char *shmaddr_selection;
-
-int action = 0;
-int timeout_id_1;
-int timeout_id_2;
-int CanNewWin = 0;
-
-int shmid_google;
-int shmid_baidu;
-int shmid_selection;
 
 int main(int argc, char **argv)
 {
-    pthread_t t1, t2, t3;
-    struct Arg arg;
 
-    char *addr_google;
-    char *addr_baidu;
-    char *addr_selection;
+    pid_t pid;
 
-    shmid_google = shared_memory_for_google_translate(&addr_google);
-    shmid_baidu = shared_memory_for_baidu_translate(&addr_baidu);
-    shmid_selection = shared_memory_for_selection(&addr_selection);
+    if ( (pid = fork()) < 0)
+        err_exit("fork err in main");
 
-    shmaddr_google = addr_google;
-    shmaddr_baidu = addr_baidu;
-    shmaddr_selection = addr_selection;
+    void (*quickSearchProcess)(void);
+    quickSearchProcess = quickSearch;
 
-    memset(shmaddr_google, '0', (size_t)10);
-    memset(shmaddr_baidu, '0', (size_t)10);
+    void (*tranSelectProcess)(void);
+    tranSelectProcess = tranSelect;
 
-    memset(shmaddr_google, '\0', SHMSIZE-10);
-    memset(shmaddr_baidu, '\0', SHMSIZE-10);
-
-    arg.addr_google = shmaddr_google;
-    arg.addr_baidu = shmaddr_baidu;
-    arg.argc = argc;
-    arg.argv = argv;
-
-    /*启动鼠标动作检测线程*/
-    pthread_create(&t2, NULL, DetectMouse, (void*)&arg);
-
-    void *thread_ret;
-
-
-    /* 捕获终止(Terminate)程序信号，并调用退出函数清理相关资源,
-     * 如释放内存，终止翻译端程序*/
-    struct sigaction sa;
-    sa.sa_handler = quit;
-    sigemptyset ( &sa.sa_mask );
-    if ( sigaction ( SIGTERM, &sa, NULL ) == -1) {
-        printf("\033[0;31msigaction exec failed (Main.c -> SIGTERM) \033[0m\n");
-        perror("sigaction");
-        exit(1);
-    }
-
-    signal(SIGINT, quit);
-
-    while (1) {
-
-        /*启动翻译入口图标线程*/
-        pthread_create(&t1, NULL, GuiEntrance, (void*)&arg);
-        pthread_join(t1, &thread_ret);
-
-        if ( CanNewWin == 0 )
-            continue;
-
-        pthread_create(&t3, NULL, newNormalWindow, NULL);
-        pthread_join(t3, &thread_ret);
-
-        CanNewWin = 0;
-    }
-
-    /*TODO:
-     * The following codes will never be executed,
-     * remember to handle it*/
-    pthread_join(t2, &thread_ret); 
+    /* 启动取词翻译进程和quickSearch进程*/
+    if ( pid > 0 )
+        tranSelectProcess();
+    else
+        quickSearchProcess();
 }
