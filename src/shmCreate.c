04e6d7538157bfd0981e108e39391c289bae11cc
poemdistance <poemdistance@gmail.com>
2019-09-15 14:16:46 +0800

    Add new functions to create new shared memory

--- src/shmCreate.c
+++ src/shmCreate.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "quickSearch.h"
 
 int shared_memory_for_google_translate(char **addr) {
 
@@ -10,7 +11,7 @@ int shared_memory_for_google_translate(char **addr) {
     shmid = shmget(key, SHMSIZE, IPC_CREAT | IPC_EXCL | 0664);
     if ( shmid == -1 ) {
         if ( errno == EEXIST ) {
-            printf("\033[0;31mshared memeory already exist,\033[0m");
+            printf("\033[0;31mshared memeory already exist, \033[0m");
             shmid = shmget(key ,0, 0);
             printf("\033[0;35mreference shmid = %d\033[0m\n", shmid);
         } else {
@@ -52,7 +53,7 @@ int shared_memory_for_baidu_translate(char **addr) {
     shmid = shmget(key, SHMSIZE, IPC_CREAT | IPC_EXCL | 0664);
     if ( shmid == -1 ) {
         if ( errno == EEXIST ) {
-            printf("\033[0;31mshared memeory already exist,\033[0m");
+            printf("\033[0;31mshared memeory already exist, \033[0m");
             shmid = shmget(key ,0, 0);
             printf("\033[0;35mreference shmid = %d\033[0m\n", shmid);
         } else {
@@ -93,7 +94,89 @@ int shared_memory_for_selection(char **addr) {
     shmid = shmget(key, 10, IPC_CREAT | IPC_EXCL | 0664);
     if ( shmid == -1 ) {
         if ( errno == EEXIST ) {
-            printf("\033[0;31mshared memeory already exist,\033[0m");
+            printf("\033[0;31mshared memeory already exist, \033[0m");
+            shmid = shmget(key ,0, 0);
+            printf("\033[0;35mreference shmid = %d\033[0m\n", shmid);
+        } else {
+            perror("errno");
+            err_exit("shmget error");
+        }
+    } else {
+        printf("\033[0;35mObtained shared memory successful shmid=%d \033[0m\n", shmid);
+    }
+
+    /* Do not to specific the address to attach
+     * and attach for read & write*/
+    if ( (*addr = shmat(shmid, 0, 0) ) == (void*)-1) {
+        if (shmctl(shmid, IPC_RMID, NULL) == -1)
+            err_exit("shmctl error");
+        else {
+            printf("Attach shared memory failed\n");
+            printf("remove shared memory identifier successful\n");
+        }
+
+        err_exit("shmat error");
+    } else {
+        printf("\033[0;35mAttach to %p \033[0m\n", addr);
+    }
+
+    printf("\n");
+
+    return shmid;
+}
+
+int shared_memory_for_quickSearch(char **addr) {
+
+    key_t key = ftok(GETEKYDIR, PROJECTID2+2);
+    if ( key < 0 )
+        err_exit("ftok error");
+
+    int shmid;
+    shmid = shmget(key, SHMSIZE, IPC_CREAT | IPC_EXCL | 0664);
+    if ( shmid == -1 ) {
+        if ( errno == EEXIST ) {
+            printf("\033[0;31mshared memeory already exist, \033[0m");
+            shmid = shmget(key ,0, 0);
+            printf("\033[0;35mreference shmid = %d\033[0m\n", shmid);
+        } else {
+            perror("errno");
+            err_exit("shmget error");
+        }
+    } else {
+        printf("\033[0;35mObtained shared memory successful shmid=%d \033[0m\n", shmid);
+    }
+
+    /* Do not to specific the address to attach
+     * and attach for read & write*/
+    if ( (*addr = shmat(shmid, 0, 0) ) == (void*)-1) {
+        if (shmctl(shmid, IPC_RMID, NULL) == -1)
+            err_exit("shmctl error");
+        else {
+            printf("Attach shared memory failed\n");
+            printf("remove shared memory identifier successful\n");
+        }
+
+        err_exit("shmat error");
+    } else {
+        printf("\033[0;35mAttach to %p \033[0m\n", addr);
+    }
+
+    printf("\n");
+
+    return shmid;
+}
+
+int shared_memory_for_keyboard_event(char **addr) {
+
+    key_t key = ftok(GETEKYDIR, PROJECTID2+3);
+    if ( key < 0 )
+        err_exit("ftok error");
+
+    int shmid;
+    shmid = shmget(key, 100, IPC_CREAT | IPC_EXCL | 0664);
+    if ( shmid == -1 ) {
+        if ( errno == EEXIST ) {
+            printf("\033[0;31mshared memeory already exist, \033[0m");
             shmid = shmget(key ,0, 0);
             printf("\033[0;35mreference shmid = %d\033[0m\n", shmid);
         } else {
