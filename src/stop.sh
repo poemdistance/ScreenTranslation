ac4fda98ea2e0058547d37d8dc2015b1c79a3224
poemdistance <poemdistance@gmail.com>
2019-11-22 19:16:23 +0800

    Add command of killing fetchDict

--- src/stop.sh
+++ src/stop.sh
@@ -7,5 +7,6 @@ do
     kill -9 `ps -aux | grep stran | head -n 1 |awk '{print $2}'|xargs`>/dev/null 2>&1
     kill -9 `ps -aux | grep tranen | head -n 1 |awk '{print $2}'|xargs`>/dev/null 2>&1
     kill -9 `ps -aux | grep bdtran | head -n 1 |awk '{print $2}'|xargs`>/dev/null 2>&1
+    kill -9 `ps -aux | grep fetchDict | head -n 1 |awk '{print $2}'|xargs`>/dev/null 2>&1
 done
 
