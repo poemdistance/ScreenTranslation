7b992a1f92694f53de22c35a8c9d548d372abb9c
poemdistance <poemdistance@gmail.com>
2019-11-25 17:08:41 +0800

    Fix the error recognized of screenshot action of mouse

--- src/tranPicActionDetect.c
+++ src/tranPicActionDetect.c
@@ -33,11 +33,10 @@ void readChild() {
 
 int detectTranPicAction () {
 
-    printf("\033[0;34m 执行截图动作检测函数 \033[0m\n");
-
     int count = 0;
     int canShot = 0;
     int lock = 1;
+    int drop = 0;
     int history_x = 0;
     int history_y = 0;
     int check_x = 0;
@@ -82,81 +81,78 @@ int detectTranPicAction () {
         while ( 1 ) {
 
 
-            XQueryPointer(display, DefaultRootWindow(display), &root_window, &root_window, &root_x, &root_y, &root_x, &root_y, &mask); 
+            XQueryPointer(display, DefaultRootWindow(display), &root_window,\
+                    &root_window, &root_x, &root_y, &root_x, &root_y, &mask); 
 
             gettimeofday ( &now, NULL );
             time1 = ( now.tv_usec + now.tv_sec*1000000 ) / 1000; /*  Unit:ms*/
-            if ( (abs ( time1 - lastTime ) > TIMEOUT) )  {
+            if ( drop || (abs ( time1 - lastTime ) > TIMEOUT) )  {
                 count = 0;
                 lastTime = 0;
                 history_x = -100;
                 history_y = -100;
+                drop = 0;
             }
 
 
             /* mask>0保证有按键按下才进入执行逻辑
              * lock保证按键按下后此处逻辑不会被反复执行，直到按键释放lock=1*/
-            if ( mask > 0 && lock ) {
+            if ( mask > 0 && lock && mask ==  Button1MotionMask ) {
 
-                if ( mask ==  Button1MotionMask ) {
 
-                    if ( (history_x != root_x || history_y != root_y) ) {
+                if ( (history_x != root_x || history_y != root_y) ) {
 
-                        gettimeofday ( &now, NULL );
-                        time1 = ( now.tv_usec + now.tv_sec*1000000 ) / 1000;  /* Unit:ms*/
+                    gettimeofday ( &now, NULL );
+                    time1 = ( now.tv_usec + now.tv_sec*1000000 ) / 1000;  /* Unit:ms*/
 
-                        /* lastTime==0时, 此次点击作第一次点击看待,去else执行, 否则进if*/
-                        if ( (int)lastTime != 0 )
-                        {
-                            if ( check_x && abs(root_x-history_x) < 33  && history_x > 0) {
+                    /* lastTime==0时, 此次点击作第一次点击看待,去else执行, 否则进if*/
+                    if ( lastTime > 0 )
+                    {
+                        if ( check_x && abs(root_x-history_x) < 21 ) {
 
-                                printf("\033[0;31mx坐标间隔连续小于33 \033[0m\n");
-                                
-                                count = 0;
-                                check_x  =0;
-                            }
-
-                            if ( check_y && abs(root_y-history_y) < 33  && history_y > 0) {
-
-                                printf("\033[0;31my坐标间隔连续小于33 \033[0m\n");
-
-                                count = 0;
-                                check_y  =0;
-                            }
-
-                            if ( abs(root_x-history_x) < 33 )
-                                check_x = 1;
-
-                            if ( abs(root_y-history_y) < 33 )
-                                check_y = 1;
-
-                            if ( abs ( time1 - lastTime ) > TIMEOUT ) {
-
-                                count = 0;
-                                lastTime = 0;
-                            }
-                            else if ( ++count == 3 )
-                                canShot = 1;
+                            printf("第3次点击x距离依旧过小，抛弃\n");
+                            drop = 1;
+                            check_x = 0;
+                            continue;
                         }
-                        else 
-                            ++count;/* 因此次点击作为第一次点击，所以计数值count加1*/
 
-                        /* 更新`上一次点击`的时间戳*/
-                        lastTime = time1;
+                        if ( check_y && abs(root_y-history_y) < 21 ) {
 
+                            printf("第3次点击y距离依旧过小，抛弃\n");
+                            drop = 1;
+                            check_y = 0;
+                            continue;
+                        }
+
+                        if ( abs(root_x-history_x) < 21 && count == 1 ) {
+                            printf("x距离过小-1\n");
+                            check_x = 1;
+                        }
+
+                        if ( abs(root_y-history_y) < 21 && count == 1 ) {
+
+                            printf("y距离过小-1\n");
+                            check_y = 1;
+                        }
+
+
+                        if ( abs ( time1 - lastTime ) > TIMEOUT ) {
+                            printf("超时抛弃\n");
+                            drop = 1;
+                            continue;
+                        }
+                        else if ( ++count == 3 )
+                            canShot = 1;
                     }
                     else 
-                        count = 0;/* 出现原地双击，丢弃点击次数统计*/
+                        ++count;/* 因此次点击作为第一次点击，所以计数值count加1*/
+
+                    /* 更新`上一次点击`的时间戳*/
+                    lastTime = time1;
 
                 }
-                else if ( mask ==  Button3MotionMask  ) {
-
-                    printf("Press right button, coordinate: %d %d\n", root_x, root_y);
-                }
-                else if ( mask ==  Button2MotionMask  ) {
-
-                    printf("Press middle button, coordinate: %d %d\n", root_x, root_y);
-                }
+                else 
+                    count = 0;/* 出现原地双击，丢弃点击次数统计*/
 
                 /* 更新历史坐标*/
                 history_y = root_y;
@@ -174,15 +170,15 @@ int detectTranPicAction () {
             if ( (mask == 0 && canShot ) || shmaddr_pic[1] == SCREEN_SHOT ) {
 
                 count = 0;
+                check_x = 0;
+                check_y = 0;
                 canShot = 0;
-                check_y  =0;
-                check_x  =0;
                 shmaddr_pic[1] = CLEAR;
                 system("gnome-screenshot -a -B -f /home/$USER/.stran/1.png");
             }
 
             /* 休眠, 防止一直占用CPU*/
-            usleep(3000);
+            usleep(10000);
         }
     }
 
