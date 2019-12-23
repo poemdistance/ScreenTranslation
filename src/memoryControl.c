d038a557c9b0d1f4878d1d6f7e68d78f43127afb
poemdistance <poemdistance@gmail.com>
2019-12-23 14:55:03 +0800

    Add release Memory functions

--- src/memoryControl.c
+++ src/memoryControl.c
@@ -1,6 +1,7 @@
 #include "common.h"
 #include "newWindow.h"
 #include "cleanup.h"
+#include <assert.h>
 
 extern char audioOnline_en[512];
 extern char audioOnline_uk[512];
@@ -16,9 +17,22 @@ void initMemoryMysql() {
 
     for (int i=0; i<MYSQLSIZE; i++) {
 
-        mysql_result[i] = calloc(SHMSIZE / MYSQLSIZE, sizeof ( char ) );
-        if (mysql_result[i] == NULL)
-            err_exit("Error occured when calloc memory in initMemoryMysql");
+        if ( i == 2  || i == 3 ) {
+
+            mysql_result[i] = calloc(ZH_EN_TRAN_SIZE, sizeof ( char* ) );
+            assert(mysql_result[i] != NULL);
+
+            for ( int j=0; j<ZH_EN_TRAN_SIZE; j++ ) {
+                mysql_result[i][j] = calloc ( PER_SENTENCE_SIZE, sizeof(char) );
+                assert(mysql_result[i][j] != NULL);
+            }
+            continue;
+        }
+        mysql_result[i] = calloc(1, sizeof ( char* ) );
+        assert(mysql_result[i] != NULL);
+
+        mysql_result[i][0] = calloc ( SHMSIZE / MYSQLSIZE, sizeof(char) );
+        assert(mysql_result[i][0] != NULL);
     }
 }
 
@@ -26,8 +40,22 @@ void releaseMemoryMysql() {
 
     /* 释放翻译结果存储空间 <Mysql>*/
     if ( mysql_result[0] != NULL)
-        for (int i=0; i<MYSQLSIZE; i++)
-            free(mysql_result[i]);
+        for (int i=0; i<MYSQLSIZE; i++) {
+
+            if ( i == 2 || i == 3 ) {
+                for ( int j=0; j<ZH_EN_TRAN_SIZE; j++ )
+                    if ( mysql_result[i][j] ) 
+                        free ( mysql_result[i][j] );
+            }
+            else {
+
+                if ( mysql_result[i][0] )
+                    free ( mysql_result[i][0] );
+            }
+
+            if ( mysql_result[i] )
+                free ( mysql_result[i] );
+        }
 }
 
 void initMemoryTmp() {
@@ -45,6 +73,28 @@ void releaseMemoryTmp() {
         free ( tmp );
 }
 
+void releaseMemoryBaidu() {
+
+    /* 释放翻译结果存储空间 <Mysql>*/
+    if ( baidu_result[0] != NULL)
+        for (int i=0; i<MYSQLSIZE; i++) {
+
+            if ( i == 2 || i == 3 ) {
+                for ( int j=0; j<ZH_EN_TRAN_SIZE; j++ )
+                    if ( baidu_result[i][j] ) 
+                        free ( baidu_result[i][j] );
+            }
+            else {
+
+                if ( baidu_result[i][0] )
+                    free ( baidu_result[i][0] );
+            }
+
+            if ( baidu_result[i] )
+                free ( baidu_result[i] );
+        }
+}
+
 /* 初始化百度翻译结果存储空间*/
 void initMemoryBaidu() {
 
@@ -53,10 +103,24 @@ void initMemoryBaidu() {
 
     for (int i=0; i<BAIDUSIZE; i++) {
 
-        baidu_result[i] = calloc(SHMSIZE / BAIDUSIZE, sizeof ( char ) );
-        if (baidu_result[i] == NULL)
-            err_exit("Error occured when calloc memory in initMemoryBaidu");
+        if ( i == 2  || i == 3 ) {
+
+            baidu_result[i] = calloc(ZH_EN_TRAN_SIZE, sizeof ( char* ) );
+            assert(baidu_result[i] != NULL);
+
+            for ( int j=0; j<ZH_EN_TRAN_SIZE; j++ ) {
+                baidu_result[i][j] = calloc ( PER_SENTENCE_SIZE, sizeof(char) );
+                assert(baidu_result[i][j] != NULL);
+            }
+            continue;
+        }
+        baidu_result[i] = calloc(1, sizeof ( char* ) );
+        assert(baidu_result[i] != NULL);
+
+        baidu_result[i][0] = calloc ( SHMSIZE / BAIDUSIZE, sizeof(char) );
+        assert(baidu_result[i][0] != NULL);
     }
+
 }
 
 /* 类上*/
@@ -81,10 +145,38 @@ void releaseMemoryGoogle() {
             free(google_result[i]);
 }
 
+void clearBaiduMysqlResultMemory() {
+
+    if ( ! baidu_result[0] )
+        return;
+
+    for ( int i=0; i<BAIDUSIZE; i++ ) {
+        if ( i == 2 || i == 3 ) {
+            for ( int j=0; j<ZH_EN_TRAN_SIZE; j++ ) {
+                if ( baidu_result[i][j] )
+                    memset ( baidu_result[i][j], '\0',  PER_SENTENCE_SIZE );
+                if ( mysql_result[i][j] )
+                    memset ( mysql_result[i][j], '\0',  PER_SENTENCE_SIZE );
+            }
+        }
+        else {
+            if ( baidu_result[i][0] ) {
+                memset ( baidu_result[i][0], '\0', SHMSIZE / BAIDUSIZE );
+            }
+            if ( mysql_result[i][0] ) {
+
+                memset ( mysql_result[i][0], '\0', SHMSIZE / MYSQLSIZE );
+            }
+        }
+    }
+}
+
 void clearMemory () {
 
-    memset ( tmp, '0', 10 );
-    memset ( tmp, '\0', SHMSIZE-10);
+    if ( tmp ) {
+        memset ( tmp, '0', 10 );
+        memset ( tmp, '\0', SHMSIZE-10);
+    }
 
     /* 标志位空间用字符0填充*/
     memset(shmaddr_baidu, '0', 10);
@@ -102,13 +194,7 @@ void clearMemory () {
     memset ( audio_en(OFFLINE), '\0', 512 );
     memset ( audio_uk(OFFLINE), '\0', 512 );
 
-    for ( int i=0; i<BAIDUSIZE; i++ )
-        if ( baidu_result[i] != NULL)
-            memset( baidu_result[i], '\0', SHMSIZE / BAIDUSIZE );
-
-    for ( int i=0; i<MYSQLSIZE; i++ )
-        if ( mysql_result[i] != NULL)
-            memset( mysql_result[i], '\0', SHMSIZE / MYSQLSIZE );
+    clearBaiduMysqlResultMemory();
 
     for ( int i=0; i<GOOGLESIZE; i++ )
         if ( google_result[i] != NULL)
--- src/memoryControl.h
+++ src/memoryControl.h
@@ -1,6 +1,9 @@
 #ifndef __INIT_MEMORY__
 #define __INIT_MEMORY__
 
+#define ZH_EN_TRAN_SIZE 20
+#define PER_SENTENCE_SIZE (1024*1024)
+
 void initMemoryMysql();
 void initMemoryBaidu();
 void initMemoryGoogle();
@@ -8,6 +11,7 @@ void initMemoryTmp();
 
 void releaseMemoryGoogle();
 void releaseMemoryMysql();
+void releaseMemoryBaidu();
 void releaseMemoryTmp();
 
 void clearMemory();
