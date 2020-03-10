27545d6b093f318a3cad1a6176d72fbd0917bf52
poemdistance <poemdistance@gmail.com>
2020-03-11 00:57:13 +0800

    Remove notification in startup.sh

--- src/errNotification.sh
+++ src/errNotification.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-notify-send 'ScreenTranslation' '取词翻译运行异常退出，请检查网络连接是否正常' --icon=dialog-information
+notify-send 'ScreenTranslation' '取词翻译运行异常退出' --icon=dialog-information
 
--- src/startup.sh
+++ src/startup.sh
@@ -2,4 +2,4 @@
 
 bash /home/$USER/.stran/stop.sh $$
 (nohup /usr/bin/mstran >>/var/log/mstran.log 2>&1) &
-notify-send 'ScreenTranslation' '取词翻译正在运行' --icon=dialog-information
+# notify-send 'ScreenTranslation' '取词翻译正在运行' --icon=dialog-information
