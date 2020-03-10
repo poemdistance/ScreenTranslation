c2867e0660d80f4002686f5e4b40961f2d4d61c7
poemdistance <poemdistance@gmail.com>
2020-03-11 00:14:52 +0800

    Make shortcuts for play audio, switch translation source... configurable

--- src/keyPressEvent.c
+++ src/keyPressEvent.c
@@ -4,6 +4,9 @@
 #include "audio.h"
 #include "newWindow.h"
 #include "windowData.h"
+#include "configControl.h"
+#include "useful.h"
+#include "configControl.h"
 
 /* 键值掩码在gdk/gdkkeysyms.h*/
 
@@ -12,23 +15,39 @@ extern char audioOnline_uk[512];
 extern char audioOffline_en[512];
 extern char audioOffline_uk[512];
 
-gboolean key_press ( GtkWidget *window, GdkEventKey *event, gpointer *data ) {
+gboolean on_key_press_cb ( 
+        GtkWidget *window,
+        GdkEventKey *event,
+        gpointer *data ) {
+
+    int mask = event->state;
+    int keyval = event->keyval;
+    int upperKeyval = gdk_keyval_to_upper ( keyval );
+
+    gboolean enableCtrlCToClose = FALSE;
+    ConfigData *cd = WINDATA(data)->cd;
+
+    enableCtrlCToClose = cd->ctrlCToClose;
+
+    pbblue ( "Key Press mask & keyval upperkeyval %d %d %d ",
+            mask, keyval, upperKeyval );
 
     if ( event->state ==  GDK_CONTROL_MASK ) {
-        if ( event->keyval == GDK_KEY_c) {
+        if ( event->keyval == GDK_KEY_c && enableCtrlCToClose ) {
 
-            g_print ("Captured Control-C, destroying window\n");
+            g_print ("Captured Control+C, destroying window\n");
             destroyNormalWin ( window, (WinData*)data );
         }
     }
 
-    if ( event->keyval == GDK_KEY_Return ) {
-        g_print ("Key Return\n");
+    if ( ((cd->switchSourceMask & mask) || cd->switchSourceMask == mask ) && 
+            (cd->switchSourceKeyval == keyval || cd->switchSourceKeyval == upperKeyval) ) {
+
         changeDisplay(GET_BUTTON(data, WINDATA(data)->who), data);
     }
 
-    if ( event->keyval == GDK_KEY_space ) {
-        g_print ("Press Space (keyPress.c)\n");
+    if ( (cd->playAudioKeyval == keyval || cd->playAudioKeyval == upperKeyval  ) &&
+            ((cd->playAudioKeyval & mask) || mask == cd->playAudioMask) ) {
 
         if ( (strlen(audioOnline_en)==0 && strlen(audioOnline_uk)==0) \
                 && (strlen(audioOffline_en)==0 && strlen(audioOffline_uk)==0) ) {
