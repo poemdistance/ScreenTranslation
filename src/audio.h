d71bc36e8c4b02d7536486d101f9f19c803547bf
poemdistance <poemdistance@gmail.com>
2020-03-29 10:36:34 +0800

    Change the prototype of function:mp3play

--- src/audio.h
+++ src/audio.h
@@ -3,17 +3,6 @@
 
 #include "windowData.h"
 
-#define AUDIO_BASE_NAME "audioButtonPosition"
-#define WIN_SIZE_BASE_NAME "winSizeInfo"
-
-int mp3play (GtkWidget *button, gpointer *data);
-
-GtkWidget* newAudioBtn () ;
-
-GtkWidget* insertAudioIcon\
-        ( GtkWidget *window, GtkWidget *layout, WinData *wd, int type ) ;
-
-void syncAudioBtn ( WinData *wd, int type );
-int getButtonPosition ( int x );
+int mp3play (GtkWidget *button, WinData *wd);
 
 #endif
