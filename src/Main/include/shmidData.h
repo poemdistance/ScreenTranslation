54ed6dbdb15aaf7ec8e235e1dcf50e10df962374
poemdistance <poemdistance@gmail.com>
2020-04-17 11:36:51 +0800

    Define ShmData structure to store shared memory id variables

---
+++ src/Main/include/shmidData.h
@@ -0,0 +1,14 @@
+#ifndef __SHMID_DATA__
+#define __SHMID_DATA__
+
+typedef struct {
+
+    pid_t pid_mysql;
+    pid_t pid_google;
+    pid_t pid_bing;
+    pid_t pid_tranpic;
+    pid_t pid_selection;
+
+}ShmIdData;
+
+#endif
