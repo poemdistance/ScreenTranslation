2a06db555f5d93f58d49b1699b358bf4967fce54
poemdistance <poemdistance@gmail.com>
2020-04-17 12:00:41 +0800

    Modify macro ( Need to get data from strcuture instead from global varibals )

--- src/Main/include/newWindow.h
+++ src/Main/include/newWindow.h
@@ -15,45 +15,28 @@ GtkWidget *newGoogleButton ( WinData *win );
 
 void insertCalibrationButton( WinData *win );
 
-extern char *text;
-
-extern char **baidu_result[BAIDUSIZE];
-extern char *google_result[GOOGLESIZE];
-extern char **mysql_result[MYSQLSIZE];
-
-extern char *tmp;
-
-extern volatile sig_atomic_t InNewWin;
-extern char *shmaddr_google;
-extern char *shmaddr_baidu;
-extern char *shmaddr_keyboard;
-extern char *shmaddr_mysql;
-extern char *shmaddr_pic;
-extern char *shmaddr_setting;
-
 #define WINDATA(addr) ((WinData*)addr)
 
-#define AUDIO_EN(type)  ( type == ONLINE ? ( audioOnline_en ) : ( audioOffline_en ))
-#define AUDIO_AM(type)  ( type == ONLINE ? ( audioOnline_am ) : ( audioOffline_am ))
+#define AUDIO_EN(AudioData, type)  ( type == ONLINE ? ( AudioData->audioOnline_en ) : ( AudioData->audioOffline_en ))
+#define AUDIO_AM(AudioData, type)  ( type == ONLINE ? ( AudioData->audioOnline_am ) : ( AudioData->audioOffline_am ))
 
-#define SHMADDR_TYPE(type) ( type == ONLINE ? ( shmaddr_baidu ) : ( shmaddr_mysql ))
-#define PhoneticFlag(type) ( type == ONLINE ? ( shmaddr_baidu[1] - '0' ) : ( shmaddr_mysql[1] - '0' ))
-#define NumZhTranFlag(type) ( type == ONLINE ? ( shmaddr_baidu[2] - '0' ) : ( shmaddr_mysql[2] - '0' ))
-#define NumEnTranFlag(type) ( type == ONLINE ? ( shmaddr_baidu[3] - '0' ) : ( shmaddr_mysql[3] - '0' ))
-#define OtherWordFormFlag(type) ( type == ONLINE ? ( shmaddr_baidu[4] - '0' ) : ( shmaddr_mysql[4] - '0' ))
-#define NumAudioFlag(type) ( type == ONLINE ? ( shmaddr_baidu[5] - '0' ) : ( shmaddr_mysql[5] - '0' ))
+/* #define SHMADDR_TYPE(type) ( type == ONLINE ? ( shmaddr_bing ) : ( shmaddr_mysql )) */
+/* #define PhoneticFlag(type) ( type == ONLINE ? ( shmaddr_bing[1] - '0' ) : ( shmaddr_mysql[1] - '0' )) */
+/* #define NumZhTranFlag(type) ( type == ONLINE ? ( shmaddr_bing[2] - '0' ) : ( shmaddr_mysql[2] - '0' )) */
+/* #define NumEnTranFlag(type) ( type == ONLINE ? ( shmaddr_bing[3] - '0' ) : ( shmaddr_mysql[3] - '0' )) */
+/* #define OtherWordFormFlag(type) ( type == ONLINE ? ( shmaddr_bing[4] - '0' ) : ( shmaddr_mysql[4] - '0' )) */
+/* #define NumAudioFlag(type) ( type == ONLINE ? ( shmaddr_bing[5] - '0' ) : ( shmaddr_mysql[5] - '0' )) */
 
-#define SourceInput(type) ( type == ONLINE ? (( char *) baidu_result[0][0]) : ((char*)mysql_result[0][0]))
-#define Phonetic(type)( type == ONLINE ? (( char *) baidu_result[1][0]) : ((char*)mysql_result[1][0]))
-#define ZhTrans(type, i) ( type == ONLINE ? (( char *) baidu_result[2][i]) : ((char*)mysql_result[2][i]))
-#define EnTrans(type) ( type == ONLINE ? (( char *) baidu_result[3][0]) : ((char*)mysql_result[3][0]))
-#define OtherWordForm(type) ( type == ONLINE ? (( char *) baidu_result[4][0]) : ((char*)mysql_result[4][0]))
-#define Audios(type) ( type == ONLINE ? (( char *) baidu_result[5][0]) : ((char*)mysql_result[5][0]))
+#define SourceInput(MemoryData, type) ( type == ONLINE ? (( char *) MemoryData->bing_result[0][0]) : ((char*)MemoryData->mysql_result[0][0]))
+/* #define Phonetic(type)( type == ONLINE ? (( char *) bing_result[1][0]) : ((char*)mysql_result[1][0])) */
+/* #define ZhTrans(type, i) ( type == ONLINE ? (( char *) bing_result[2][i]) : ((char*)mysql_result[2][i])) */
+/* #define EnTrans(type) ( type == ONLINE ? (( char *) bing_result[3][0]) : ((char*)mysql_result[3][0])) */
+/* #define OtherWordForm(type) ( type == ONLINE ? (( char *) bing_result[4][0]) : ((char*)mysql_result[4][0])) */
+/* #define Audios(type) ( type == ONLINE ? (( char *) bing_result[5][0]) : ((char*)mysql_result[5][0])) */
 
 
 int destroyNormalWin(GtkWidget *window, WinData *win);
 int waitForContinue();
-int getIndex(int *index, char *addr);
 void printDebugInfo();
 int  newScrolledWin();
 void setFontProperties(GtkTextBuffer *buf, GtkTextIter *iter);
@@ -72,10 +55,10 @@ gboolean on_key_press_cb ( GtkWidget *window, GdkEventKey *event, WinData *wd );
 void *newNormalWindow ( void *data );
 void adjustStrForGoogle(char *p[3], int len, char *storage[3], int *enterNum);
 
-void separateDataForBaidu(int *index, int len, int type);
+void separateDataForBaidu(int *index, int len, int type, WinData *wd );
 void adjustStrForBaidu(int len, char *source, int addSpace, int copy, int *enterNum);
 int getLinesOfGoogleTrans ( int *index_google );
-void separateGoogleData ( int *index_google, int len );
+void separateGoogleData ( int *index_google, int len, WinData *wd );
 int countCharNums ( char *source );
 
 #endif
