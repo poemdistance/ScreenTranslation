a2f141dd75b9286c59b9fff81b85bd53d2b1abc8
poemdistance <poemdistance@gmail.com>
2020-04-17 11:54:31 +0800

    Rename macro

--- src/Main/include/memory.h
+++ src/Main/include/memory.h
@@ -6,8 +6,8 @@
 typedef struct {
 
     char **bing_result[BING_SIZE];
-    char *google_result[GOOGLESIZE];
-    char **mysql_result[MYSQLSIZE];
+    char *google_result[GOOGLE_SIZE];
+    char **mysql_result[MYSQL_SIZE];
 
     char *tmp;
 
